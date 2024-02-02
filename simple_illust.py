a235f6146e6cfa56a5524793ae882d380c8b6b59
ksyint <82583462+ksyint@users.noreply.github.com>
2024-02-03 02:30:23 +0900

    Create simple_illust.py

---
+++ simple_illust.py
@@ -0,0 +1,21 @@
+from diffusers import StableDiffusionInpaintPipeline
+import torch
+from PIL import Image 
+from segment import segmentation
+
+
+pipe = StableDiffusionInpaintPipeline.from_pretrained(
+    "ksyint/simple_illust",
+    torch_dtype=torch.float16,
+)
+pipe.to("cuda")
+
+image=Image.open("input.png")
+image,mask_image=segmentation(image)
+
+prompt="\u003Csimpleillust-background> advertisement"
+
+negative_prompt="worst quality, letters or words are written on the background, english is displayed, shoe is displayed"
+
+image = pipe(prompt=prompt,negative_prompt=negative_prompt, image=image, mask_image=mask_image).images[0]
+image.save("output.png")
